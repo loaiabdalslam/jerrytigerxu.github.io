9e045ab48014cce45ac1013a528d88b27ca34d41
Jere Xu <jtxu2008@gmail.com>
2019-05-24 12:15:04 -0400

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,33 +1,19 @@
 // Wrap every letter in a span
-$('.ml11 .letters').each(function(){
+$('.ml9 .letters').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [0,$(".ml11 .letters").width()],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
     delay: function(el, i) {
-      return 34 * (i+1)
+      return 45 * (i+1)
     }
   }).add({
-    targets: '.ml11',
+    targets: '.ml9',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
